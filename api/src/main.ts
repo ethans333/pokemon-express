e845938ea33fe349c26ea40f96b0c3f9e0916007
estein <ethanmstn33@gmail.com>
2025-06-23 12:45:11 -0400

    Enabled cors for express

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import { pool } from "./db.ts";
 import type { Pokemon } from "./types/Pokemon.ts";
@@ -7,6 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 // Get information
 app.get("/info", async (req, res) => {
