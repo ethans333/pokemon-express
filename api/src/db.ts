fe0e45a88ae34015f2fd2b55c05ffc336b7e92b4
estein <ethanmstn33@gmail.com>
2025-06-23 11:30:53 -0400

    Configuring host name of database container

--- api/src/db.ts
+++ api/src/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   user: "postgres",
-  host: "localhost",
+  host: "db-service",
   database: "pokemondb",
   password: "password",
   port: 5432,
